98c7408600e0babf291eb4caad133c8e72dda14e
tun_myat <mctiem.mgy@gmail.com>
2024-10-23 13:43:22 +0630

    initial commit

---
+++ src/main/java/com/tiem/spring_validating_form_input/SpringValidatingFormInputApplication.java
@@ -0,0 +1,13 @@
+package com.tiem.spring_validating_form_input;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidatingFormInputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringValidatingFormInputApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiem/spring_validating_form_input/SpringValidatingFormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiem.spring_validating_form_input;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidatingFormInputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
